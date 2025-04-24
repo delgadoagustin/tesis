39b81aef148c092d57962be634ca74ddd6087ff8
Agustin Delgado <delgadoagustin147@gmail.com>
2025-04-24 14:57:59 -0300

    Load labels and PCA

--- data_load.py
+++ data_load.py
@@ -23,3 +23,25 @@ def get_pulses_df(pulses_folder: str, file_amount: int=0) -> pd.DataFrame:
     
     return pulses_df
 
+def get_labels_df(labels_folder: str, file_amount: int=0) -> pd.DataFrame:
+    """
+    Returns a dataframe with all labels data
+    """
+    
+    # Get all labels csv files
+    labels_files = [
+        os.path.join(root, file)
+        for root, _, files in os.walk(labels_folder)
+        for file in files
+        if file.endswith(".csv.gz")
+    ]
+    
+    # Load all labels csv files into a dataframe
+    if file_amount > 0:
+        labels_files = labels_files[:file_amount]
+    elif file_amount < 0:
+        labels_files = labels_files[file_amount:]
+    labels_df = pd.concat([pd.read_csv(file) for file in labels_files], ignore_index=True)
+    
+    return labels_df
+
